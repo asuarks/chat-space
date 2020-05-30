4b4d0e72057e5e576bf1b95549b34125841b6532
AsukaFukaumi <asuarks@outlook.com>
2020-05-30 21:29:19 +0900

    js HTML create
    6. 返ってきたJSONをdoneメソッドで受取り、HTMLを作成する
    
    7. 6で作成したHTMLをメッセージ画面の一番下に追加する
    https://master.tech-camp.in/curriculums/3818
    
    参考
    返ってきたJSONをdoneメソッドで受取り、HTMLを作成する
    https://master.tech-camp.in/curriculums/2148#5

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,60 @@
 $(function(){
-
-  $("#new_message").on("submit", function(e){
+     function buildHTML(message){
+      if ( message.image ) {
+        var html = 
+         `<div class ="chat-main__messages__box">
+            <div class ="chat-main__messages__box__up">
+              <div class ="chat-main__messages__box__up__title">
+                ${message.user_name}
+              </div>
+              <div class ="chat-main__messages__box__up__time">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class ="chat-main__messages__box__message">
+              <p class ="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        var html =
+         `<div class ="chat-main__messages__box">
+            <div class ="chat-main__messages__box__up">
+              <div class ="chat-main__messages__box__up__title">
+                ${message.user_name}
+              </div>
+              <div class ="chat-main__messages__box__up__time">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class ="chat-main__messages__box__message">
+              <p class ="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+        return html;
+      };
+    }
+$("#new_message").on("submit", function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action');
+    var url = $(this).attr('action')
     $.ajax({
       url: url,
       type: 'POST',
-      data: formData,  
+      data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__messages').append(html);
+      $('form')[0].reset();
+    })
   })
-})
+});

482676de2f3a38def00ba73b2ebddfd649e15daa
AsukaFukaumi <asuarks@outlook.com>
2020-05-30 21:44:30 +0900

    continuously submit
    9. 連続で送信ボタンを押せるようにする
    https://master.tech-camp.in/curriculums/3818#18
    
    参考
    disabled属性をfalseにする記述を追加
    https://master.tech-camp.in/curriculums/2148#6

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,6 +56,7 @@ $("#new_message").on("submit", function(e){
       $('.chat-main__messages').append(html);
       $('form')[0].reset();
       $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
+      $('.submit-btn').prop('disabled', false);
     })
   })
 });

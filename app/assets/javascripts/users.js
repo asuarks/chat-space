b582a955f9a65eb12069591d3242fa2c034bf3ae
AsukaFukaumi <asuarks@outlook.com>
2020-05-31 19:44:44 +0900

    Asynchronous at event
    4.イベント時に非同期通信できるようにする
    https://master.tech-camp.in/curriculums/3708#9
    
    解説
    https://master.tech-camp.in/curriculums/3868
    
    params[:keyword]に値が入っていればそのまま処理は続けられ、空だった場合はそこで処理が終わる。
    検索処理の内容は、whereメソッドを使用し、入力された値を含むかつ、ログインしているユーザーのidは除外するという条件で取得。

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,17 @@
 $(function(){
   $("#user-search-field").on("keyup", function(){
-    var input = $("#user-search-field").val();    //フォームの値を取得して変数に代入する
-    console.log(input);
+    let input = $("#user-search-field").val();    //フォームの値を取得して変数に代入する
+    $.ajax({
+      type: "GET",    //HTTPメソッド
+      url: "/users",  //users_controllerの、indexアクションにリクエストの送信先を設定する
+      dataType: "json",
+      data: { keyword: input }    //テキストフィールドに入力された文字を設定する
+    })
+      .done(function(users) {
+        console.log("成功です");
+      })
+      .fail(function() {
+        console.log("失敗です");
+      });
   });
 });

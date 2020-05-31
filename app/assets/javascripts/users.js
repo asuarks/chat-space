21f997f4cc85a2b2628d9ffcf29197f7fbe3f61a
AsukaFukaumi <asuarks@outlook.com>
2020-05-31 21:01:46 +0900

    append setting for HTML display
    6.作成したHTMLをビュー上に描画する
    https://master.tech-camp.in/curriculums/3708#11
    
    参考
    非同期通信の結果を得て、HTMLを作成
    append
    https://master.tech-camp.in/curriculums/3707#11

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,14 +5,17 @@ $(function(){
         <p class="chat-group-user__name">${user.name}</p>
         <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
       </div>
-    `
+    `;
+    $("#user-search-result").append(html);
+    console.log(html)
   }
   function addNoUser() {
     let html = `
       <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">ユーザーが見つかりません</p>
       </div>
-    `
+    `;
+    $("#user-search-result").append(html);
   }
 
   $("#user-search-field").on("keyup", function(){
@@ -24,11 +27,11 @@ $(function(){
       data: { keyword: input }
     })
       .done(function(users) {
-        $("#user-search-result").empty(); //emptyメソッドで一度検索結果を空にする
+        $("#user-search-result").empty();
 
-        if (users.length !== 0) {        //usersが空かどうかで条件分岐
-          users.forEach(function(user) { //配列オブジェクト１つ１つに対する処理
-            addUser(user);               //引数に値が入っていた場合に、ビューに追加するためのHTMLを作成
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            addUser(user);
           });
         } else if (input.length == 0) {
           return false;

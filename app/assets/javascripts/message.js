9b9fe6a730e010c985e04391776bf0d46e7547be
AsukaFukaumi <asuarks@outlook.com>
2020-06-02 14:32:25 +0900

    edit message.js
    message.jsを編集しよう
    https://master.tech-camp.in/curriculums/1211#13

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,4 @@
 $(function(){
-  var last_message_id = $('.chat-main__messages__box:last').data("message-id");
-  console.log(last_message_id);
  
      function buildHTML(message){
       if ( message.image ) {
@@ -67,4 +65,25 @@ $("#new_message").on("submit", function(e){
       $('.submit-btn').prop('disabled', false);
     })
     })
+
+    var reloadMessages = function() {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      var last_message_id = $('.chat-main__messages__box:last').data("message-id");
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: "api/messages",
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        console.log('success');
+      })
+      .fail(function() {
+        alert('error');
+      });
+    };
+
 });
